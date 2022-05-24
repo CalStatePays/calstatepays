af0ce70d89fb0d1d423fb807bd47320c0e86e129
luisjg <contact@luisjg.io>
2022-05-23 20:22:10 -0700

    Copy edits.

--- resources/src/js/router/views/about/index.vue
+++ resources/src/js/router/views/about/index.vue
@@ -1,5 +1,6 @@
 <template>
 		<div class="container">
+			<h2 class="text-center">About</h2>
 	    <div class="row">
 	      <div class="col-xs-6 col-sm-8 col-md-4">
 	        <a href="https://academics.csun.edu/faculty/richard.moore" target="_blank">
@@ -24,39 +25,97 @@
 	    </div>
 
 	    <div class="row">
-	      <div class="col-xs-6 col-sm-8 col-md-4">
-	        <a href="//www.csun.edu/academic-affairs/meta-lab" target="csun">
-	          <img class="metalogo" :src="metaSvg" :alt="`META+LAB`" />
-	        </a>
-	      </div>
-	      <p class="col-xs-6 col-sm-4 col-md-4 text-center">
-	        Steve Fitzgerald - Director
-	        <br />Yazmin Peebles - Assistant Director
-	        <br />Luis J. Guzman - Project Manager
-	        <br />Alec N. Marcum - Scrum Master
-	        <br />Mikkal McNulty - Full Stack Developer
-	        <br />Jaime Garcia - UI/UX
-	        <br />Zane Ervin - Front End Developer
-	        <br />Edgar A. Cano - Front End Developer
-	        <br />Carlos Benavides - Back End Developer
-	        <br />Michael Chan - Back End Developer
-	        <br />Jorge A. Ruiz - DevOps
-	        <br />Ekene Akpati - Marketing
-	      </p>
+	    	<div class="col-xl-12">
+	    		<p>Calstatepays.org, is an interactive website that tracks the longitudinal earnings and employment of approximately 670,000 students who entered any one of six L.A.-metro CSU campuses over the last 20 years. The campuses include: California State University Northridge, California State University Dominguez Hills, California State University Channel Islands, California State University Long Beach,  California State University Fullerton  and California Polytechnic University Pomona. The purpose of the site is to help applicants, first-time freshman or transfer students, or parents understand where different majors lead in terms of earnings and employment and to help campuses understand their impact on students’ careers.</p>
+		    </div>
+		    <div class="col-xl-12">
+		      <p>The project was originally funded by the Strada Foundation and the site was launched in 2017.  The current site has data updated to 2021.  The update of CalStatepays.org, was supported by CSUN’s Provost, Mary Beth Walker.</p>
+		    </div>
+		    <div class="col-xl-12">
+		      <p>We also thank the Presidents of the six cooperating campuses and each campus’s Institutional Research Office for supporting the project and providing the seed data on enrolled students.</p>
+		    </div>
+		    <div class="col-xl-12">
+		      <h2>CalStatePays.org Project Team</h2> 
+		      <p>The CalStatePays.org project team has worked together since 2016.  It is an interdisciplinary team of faculty, consultants and students based in the College of Business and Economics at California State University Northridge (CSUN). The current update of the site included:</p>
+		      <ul>
+		        <li>Richard W. Moore, Ph.D. Professor of Management (Co-Founder and Director)</li>
+		        <li>Kenneth Chapman, Ph.D. Professor of Economics (Co-founder and Project Economist)</li>
+		        <li>Eric Iskowitz, MBA (Project Manager)</li>
+		      </ul>
+		    </div>
+		    <div class="col-xl-12">
+		      <h3>Past project staff who helped develop the original site</h3>
+		        <ul>
+		          <li>Catherine Quin Hou, former Project Manager</li>
+		          <li>Harry Hellenbrand, Professor Emeritus</li>
+		          <li>Betina Huber, Institutional Research Director Emeritus</li>
+		          <li>Daniel Bustos, Research Assistant</li>
+		          <li>Lissette Rojo, Research Associate</li>
+		        </ul>
+		    </div>
+		    <div class="col-xl-12">
+		      <h3>California State University Northridge, Office of Institutional Research</h3>
+					<p>The mission of the CSUN Office of Institutional Research is to support and collaborate with campus stakeholders in making data-informed decisions related to university priorities and strategic planning. In support of this mission, IR provides university data to meet reporting requirements, communicates relevant data to the campus community, offers data literacy training and education to university stakeholders, and supports university leadership by providing data and related analytic tools.</p>
+		      <ul>
+		        <li>Janet Oh, Ph.D.</li>
+		        <li>Joel Hickman</li>
+		        <li>Jeffrey Chambers</li>
+		      </ul>
+		    </div>
+		    <div class="col-xl-12">
+			    <h3>Employment Development Department (EDD), Labor Market Information Division (LMID)</h3>
+					<p>The Labor Market Information Division (LMID) is the official source for California Labor Market Information. The LMID promotes California's economic health by providing information to help people understand California's economy and make informed labor market choices. We collect, analyze, and publish statistical data and reports on California's labor force, industries, occupations, employment projections, wages and other important labor market and economic data. Andy Wong was the primary analyst with LMID who assisted the project.</p>
+		    </div>
+		    <div class="col-xl-12">
+		      <h3>CSUN Central IT Unit</h3>
+					<p>The Central IT unit gave CalStatePays.org a permanent home and assisted in updating the site in 2022. The Central IT Team included:</p>
+		      <div class="row">
+		        <div class="col-md-6">
+		          <ul>
+		            <li>Ranjit A. Philip</li>
+		            <li>Yazmin Peebles</li>
+		          </ul>
+		        </div>
+		        <div class="col-md-6">
+		          <ul>
+		            <li>Luis Guzman</li>
+		            <li>Prakash Nair</li>
+		          </ul>
+		        </div>
+		      </div>
+		    </div>
+		    <div class="col-xl-12">
+		      <h3>META+LAB</h3>
+					<p>META+LAB was an innovative organization at CSUN that used students, faculty and IT professionals to develop original solutions to data system problems. The original Website Design and supporting databases were designed by a META+LAB team that including:</p>
+			    <div class="row">
+			      <div class="col-md-6">
+			        <ul>
+			          <li>Steve Fitzgerald - Director</li>
+			          <li>Yazmin Peebles - Assistant Director</li>
+			          <li>Luis J. Guzman - Project Manager</li>
+			          <li>Alec N. Marcum - Scrum Master</li>
+			          <li>Mikkal McNulty - Full Stack Developer</li>
+			          <li>Jaime Garcia - UI/UX</li>
+			        </ul>
+		        </div>
+		        <div class="col-md-6">
+		          <ul>
+		            <li>Zane Ervin - Front End Developer</li>
+		            <li>Edgar A. Cano - Front End Developer</li>
+		            <li>Carlos Benavides - Back End Developer</li>
+		            <li>Michael Chan - Back End Developer</li>
+		            <li>Jorge A. Ruiz - DevOps</li>
+		            <li>Ekene Akpati - Marketing</li>
+		          </ul>
+		        </div>
+		      </div>
+		    </div>
 	    </div>
+	  </div>
   </div>
 </template>
 <script>
 export default {
   name: "about",
-  data() {
-    return {
-    	url: "",
-		metaSvg: window.baseUrl + "/img/otherLogos/metalab-logo_square.svg"
-    };
-  },
-  created() {
-    this.url = window.baseUrl;
-  }
 };
 </script>
--- resources/src/js/router/views/faq/index.vue
+++ resources/src/js/router/views/faq/index.vue
@@ -8,37 +8,37 @@
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">I can not find my CSU.</div>
-						<div class="p-sm-4 faq-panel">The initial study relies on students enrolled at California State University, Northridge. We are currently working on collecting data for one additional CSU campus in the Greater Los Angeles Area.</div>
+						<div class="p-sm-4 faq-panel">Currently, Calstatepays.org is limited to the following six participating CSU campuses: California State University Channel Islands, California State University Dominguez Hills, California State University Fullerton, California State University of Long Beach, California State University Northridge and Cal Poly Pomona.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">How many students were followed-up in this study?</div>
-						<div class="p-sm-4 faq-panel">Over 600,000 students, all of whom entered one of the seven participating CSU campuses as first-time freshmen, transfer students, or post-baccalaureate students between 1995 and 2014.</div>
+						<div class="p-sm-4 faq-panel">Over 670,000 students, all of whom entered one of the six participating CSU campuses as first-time freshmen, transfer students, credential or post-baccalaureate students between 1995 and 2018.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">Were all students found?</div>
-						<div class="p-sm-4 faq-panel">No. Only students who remained in California, and whose earnings were covered by Unemployment Insurance at 2, 5, and 10 years after education exit are included in the study. As a result, students who have moved out of state or are out of the labor market are excluded, as are those in the military, federal employment, or self-employed. Further, the study population was limited to students who, in any given period, had no more than two (2) consecutive quarters without reported earnings. You can view a copy of the All University Earnings Data report (.pdf), which includes the percentage of students found during the study.</div>
+						<div class="p-sm-4 faq-panel">No. Only students who remained in California, and whose earnings were covered by Unemployment Insurance at 2, 5, 10, and 15 years after education exit are included in the study. As a result, students who have moved out of state or are out of the labor market are excluded, as are those in the military, federal employment, or are self-employed. Further, the study population was limited to students who, in any given year, had no more than two (2) consecutive quarters without reported earnings.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">I can not find my major.</div>
-						<div class="p-sm-4 faq-panel">Only majors pursued by more than a minimum number of students are reported.</div>
+						<div class="p-sm-4 faq-panel">Not every major is offered by every campus. So if the major you are looking for does not appear at a specific campus it may not be offered. Additionally, only majors that meet the required minimum number of students enrolled are reported.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">I can not find an industry I’m interested in.</div>
-						<div class="p-sm-4 faq-panel">Employment data are provided by industry, in keeping with 2-digit North American Industry Classification System (NAICS) classifications, rather than by occupation. Only industries employing a minimum number of students are reported.</div>
+						<div class="p-sm-4 faq-panel">Employment data are provided by industry using the 2-digit North American Industry Classification System (NAICS) classifications, rather than by occupation. Each 2-digit industry classification contains multiple related industries, for example, the Education Industry includes both K-12 education and higher education as well as other education sectors. If you don’t see an industry in a specific table, it is because only industries employing a minimum number of students are reported.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">Are lapses in employment included?</div>
-						<div class="p-sm-4 faq-panel">Yes. However, individuals who have more than two (2) consecutive quarters without earnings in a given time period are excluded from consideration.</div>
+						<div class="p-sm-4 faq-panel">Yes. However, individuals who have more than two (2) consecutive quarters without earnings in a year are excluded from the calculations.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
@@ -50,37 +50,31 @@
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">What does "Some College" mean?</div>
-						<div class="p-sm-4 faq-panel">"Some College" indicates that a student attended the university in question, but has yet to complete a four-year degree anywhere.</div>
+						<div class="p-sm-4 faq-panel">"Some College" indicates that a student attended the university in question, but has yet to complete a four-year degree at the time of the study.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">What does "Data Not Available" mean?</div>
-						<div class="p-sm-4 faq-panel">"Data Not Available" indicates that we were not able to find any data for the major or industry at the time of the initial study.</div>
+						<div class="p-sm-4 faq-panel">"Data Not Available" indicates that we were not able to find the minimum number of data required for the major or industry at the time of the initial study.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
-						<div slot="header" class="font-weight-bold">Do the Master’s Degree data include all Bachelor’s Degree recipients?</div>
-						<div class="p-sm-4 faq-panel">No. The Master’s Degree data are confined to Bachelor’s Degree recipients who went on to pursue and complete a post-baccalaureate degree or certificate. Further, such degree recipients have been removed from the earnings data for the Bachelor’s Degree recipients.</div>
-					</v-expansion-panel-content>
-				</v-expansion-panel>
-				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
-					<v-expansion-panel-content>
-						<div slot="header" class="font-weight-bold">Why are the annual earnings data of Master’s Degree recipients in the same degree field lower than those shown for Some College and/or Bachelor's Degree recipients in some cases?</div>
-						<div class="p-sm-4 faq-panel">When the number of students in any given subgroup is quite small, their average earnings can be misleading. As a result, they may appear to be lower than those of students with less advanced degrees.</div>
+						<div slot="header" class="font-weight-bold">Are students who attained a graduate degree included in earnings data for bachelor degree recipients?</div>
+						<div class="p-sm-4 faq-panel">No. The Graduate Degree data are confined to Bachelor’s Degree recipients who went on to pursue and complete a post-baccalaureate degree or certificate. Further, such degree recipients have been removed from the earnings data for the Bachelor’s Degree recipients, so those earning figures are for individuals with only a bachelor degrees.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">Are the earnings shown adjusted for inflation?</div>
-						<div class="p-sm-4 faq-panel">Yes, they are adjusted to 2016 dollars.</div>
+						<div class="p-sm-4 faq-panel">Yes, they are adjusted to 2018 dollars.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">Where did the earnings data come from?</div>
-						<div class="p-sm-4 faq-panel">Earnings are reported for California workers whose earnings are covered by Unemployment Insurance at 2, 5, and 10 years after education exit. As a result, students who have moved out of state or are out of the labor market are excluded, as are those in the military, federal employment, or self-employed.</div>
+						<div class="p-sm-4 faq-panel">Earnings are reported for California workers who were covered by Unemployment Insurance. Earnings were measured at 2, 5, 10 and 15 years after education exit. As a result, students who have moved out of state or are out of the labor market are excluded, as are those in the military, federal employment, or self-employed.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
@@ -94,15 +88,14 @@
 						<div slot="header" class="font-weight-bold">What factors affect your Personal Financial Return on Education (PFRE)?</div>
 						<div class="p-sm-4 faq-panel">Things that influence your Personal Financial Return on Education (PFRE):
 							<ul class="pt-4">
-								<li>The more you could earn without a bachelor’s degree, the more earnings you give up while enrolled - decreasing PFRE. For example, transfer students with an associates degree from a community college have better opportunities to earn more while at CSU than a first-time freshman. The income not earned while enrolled in school is often called opportunity cost.</li>
-								<li>The shorter time you spend at CSU, the briefer the investment period, the lower the opportunity cost and, thus, the higher the PFRE.
-								<li>The more you earn while obtaining a bachelor’s degree, the less income you give up to get it - increasing PFRE. There can be other effects as well. It might take you longer to graduate (lowering PFRE). You also learn useful things on the job which can make your earnings after graduation greater.</li>
-								<li>The more financial aid money you receive (excluding loans), the less you pay to get a bachelor’s degree - increasing PFRE.
-								<li>The longer it takes you to graduate, the higher your education costs--decreasing PFRE.</li>
+								<li>NOTE: All PFRE results are for students who completed a bachelor degree only.</li>
+								<li>The more you could earn without a bachelor’s degree, the more earnings you give up while enrolled - decreasing PFRE. For example, transfer students with an associate degree from a community college have better opportunities to earn more while at CSU than a first-time freshman.  The income not earned while enrolled in school is often called opportunity cost.</li>
+								<li>The shorter time you spend at CSU, the briefer the investment period, the lower the opportunity cost and, thus, the higher the PFRE.</li>
+								<li>The more you earn while obtaining a bachelor’s degree, the less income you give up to get it--increasing PFRE. There can be other effects as well.  It might take you longer to graduate (lowering PFRE).  You also learn useful things on the job which can make your earnings after graduation greater.</li>
+								<li>The more financial aid money you receive (excluding loans), the less you pay to get a bachelor’s degree - increasing PFRE.</li>
+								<li>The longer it takes you to graduate, the higher your education costs - decreasing PFRE.</li>
 								<li>The earlier you begin work after completing your bachelor degree, the longer you receive higher post-graduation earnings, which increases your PFRE. When older people get education, they have fewer years after graduation and prior to retirement so there is a shorter period of higher earnings and a lower PFRE.</li>
 								<li>Majors that produce higher post-graduation earnings will get a higher PFRE.</li>
-								<li>Majors that send most graduates to advanced degree programs (Law Degrees, MBAs, Medical Degrees, etc.) have mixed effects on PFRE. On the one hand it can be a long time before you graduate from the advanced degree and enter the labor force – reducing PFRE. On the other hand, earning after entering the labor force are usually much higher.</li>
-
 							</ul>
 						</div>
 					</v-expansion-panel-content>
@@ -116,13 +109,13 @@
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">What is the Power Users Page?</div>
-						<div class="p-sm-4 faq-panel">This page is an extension of CalStatePays. Here users are able to see a more detailed views of reported earnings, view multiple earnings of different majors, and view comparisons based on people's backgrounds.</div>
+						<div class="p-sm-4 faq-panel">This page is an extension of CalStatePays. A link to the Power Users Page can be found near the bottom left hand corner of every page. Data are in the Tableau platform which allows you to design your own data displays. It also contains some measures not included on the main site, such as, Earning while Enrolled. You may also compare earnings by key demographic groups such Age at Entry, Gender, Pell Status and Race. You can access all six campuses combined and CSUN data without a password. To access other campuses, contact the Institutional Research Office at your campus of interest for an account and password.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 				<v-expansion-panel popout class="col-12 col-lg-6 px-3">
 					<v-expansion-panel-content>
 						<div slot="header" class="font-weight-bold">Who are Power Users?</div>
-						<div class="p-sm-4 faq-panel">Researchers that want to learn more about industrial pay, and college advisors. If prompted to log into the Tableau Server, please contact your University's Institutional Research.</div>
+						<div class="p-sm-4 faq-panel">Generally, faculty and staff from campuses who contributed data to calstatepays.org.</div>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 			</div>
